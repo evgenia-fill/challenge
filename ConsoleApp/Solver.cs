a803ba040259697230a3f72cba7d1a458e6c1a13
evgenia-fill <evgeniafil04@gmail.com>
2024-11-09 19:51:50 +0500

    init

---
+++ ConsoleApp/Solver.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Challenge.DataContracts;
+
+namespace ConsoleApp;
+
+public class Solver
+{
+    public static string Solve(TaskResponse taskResponse)
+    {
+        var line = taskResponse.Question.TrimEnd(new char[] { '\r', '\n' }).Split('\n');
+        return "";
+    }
+}
